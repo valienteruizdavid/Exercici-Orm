7cbc72a2401ead6300c8407064730e8c0ebc05d3
valienteruizdavid <valiente.ruiz.david@alumnat.copernic.cat>
2020-11-28 21:24:52 +0100

    Ejercicio Orm completado

--- src/Orm/OrmMain.java
+++ src/Orm/OrmMain.java
@@ -23,22 +23,7 @@ import java.sql.Statement;
  */
 public class OrmMain {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        
-//        Class.forName("com.mysql.jdbc.Driver");
-//        Connection co = DriverManager.getConnection("jdbc:mysql://localhost/cine", "root", "");
-//        Statement st = co.createStatement();
-//        
-//        ResultSet resultado = st.executeQuery("SELECT * FROM pelicula");
-//
-//        while (resultado.next()) {
-//            System.out.println("ID: " + resultado.getObject("idPelicula")
-//                    + ", Duracion: " + resultado.getObject("duracionPelicula")
-//                    + ", Está en 3D: " + resultado.getObject("pelicula3D")
-//                    + ", Sala: " + resultado.getObject("salaCine")
-//                    + ", Nombre de la película: " + resultado.getObject("nombrePelicula")
-//                    + ", Fecha de estreno: " + resultado.getObject("fechaEstreno"));
-//        }
-//        resultado.close();
+
 
           Registre r = new Registre("select * from pelicula","jdbc:mysql://localhost/cine" , "root", "");
           r.campos();
